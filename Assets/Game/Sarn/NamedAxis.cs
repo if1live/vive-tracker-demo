776d0d75509884152f2fbec8fb427b25c306e31f
if1live <libsora25@gmail.com>
2017-05-23 16:00:00 +0900

    axis 프리팹의 중복코드 제거

---
+++ Assets/Game/Sarn/NamedAxis.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Sarn {
+    class NamedAxis : MonoBehaviour {
+        [SerializeField]
+        string label = "Label";
+
+        void Start() {
+            this.Text = label;
+        }
+
+        public string Text
+        {
+            get { return this.label; }
+            set
+            {
+                this.label = value;
+
+                var tms = GetComponentsInChildren<TextMesh>();
+                foreach (var tm in tms) {
+                    tm.text = value;
+                }
+            }
+        }
+
+        public void SetVisible(bool b) {
+            var rs = GetComponentsInChildren<Renderer>();
+            foreach(var r in rs) {
+                r.enabled = b;
+            }
+        }
+    }
+}
---
+++ Assets/Game/Sarn/NamedAxisHolder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Game.Sarn {
+    class NamedAxisHolder : MonoBehaviour {
+        public NamedAxis axisPrefab;
+        NamedAxis axis;
+
+        public string label = "label";
+
+        private void Awake() {
+            axis = Instantiate<NamedAxis>(axisPrefab);
+            axis.Text = label;
+
+            var tr = axis.transform;
+            tr.SetParent(transform);
+            tr.localPosition = Vector3.zero;
+            tr.localRotation = Quaternion.identity;
+            tr.localScale = Vector3.one;
+        }
+    }
+}
