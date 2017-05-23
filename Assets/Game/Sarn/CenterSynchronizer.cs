b347758bdd676eb799e74d412524231d81da2a29
if1live <libsora25@gmail.com>
2017-05-23 17:11:07 +0900

    트래커와 월드 중심의 좌표 어긋내는게 가능하도록

--- Assets/Game/Sarn/CenterSynchronizer.cs
+++ Assets/Game/Sarn/CenterSynchronizer.cs
@@ -5,6 +5,13 @@ namespace Assets.Game.Sarn {
     /// center에 대응되는 vive tracker가 (0,0,0)을 가리키도록 강제 고정
     /// </summary>
     class CenterSynchronizer : MonoBehaviour {
+        [SerializeField]
+        Vector3 _offset = Vector3.zero;
+        public Vector3 Offset
+        {
+            get { return _offset; }
+        }
+
         private void Update() {
             var trackerTr = ViveTracker.Get(ViveTrackerType.Center);
             if (trackerTr == null) { return; }
@@ -14,9 +21,10 @@ namespace Assets.Game.Sarn {
             var centerTr = center.transform;
 
             var trackerPos = trackerTr.WorldPosition;
+            var centerPos = trackerPos + Offset;
 
             var centerLocalPos = centerTr.localPosition;
-            var nextCenterLocalPos = centerLocalPos - trackerPos;
+            var nextCenterLocalPos = centerLocalPos - centerPos;
 
             centerTr.localPosition = nextCenterLocalPos;
         }
